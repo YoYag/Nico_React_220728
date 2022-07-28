972f9af128696d078cb9227876aa1fb7f22ccc04
YoYag <la2034342@gmail.com>
2022-07-28 11:45:28 +0900

    변환 선택 구현 및 Long Converter 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function App() {
+function MinutesToHours() {
   const [amount, setAmount] = React.useState(0);
   const [flipped, setFlipped] = React.useState(false);
   const onChange = (event) => {
@@ -13,7 +13,7 @@ function App() {
   };
   return (
     <div>
-      <h1>Time Converter</h1>
+      <h2>Time Converter</h2>
       <div>
         <label htmlFor="minutes">Minutes</label>
         <input
@@ -40,6 +40,69 @@ function App() {
       <button onClick={onFlip}>
         {flipped ? "Hours => Minutes" : "Minutes => Hours"}
       </button>
+    </div>
+  );
+}
+
+function KmToMiles() {
+  const [amount, setAmount] = React.useState(0);
+  const [flipped, setFlipped] = React.useState(false);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const reset = () => setAmount(0);
+  const onFlip = () => {
+    reset();
+    setFlipped((current) => !current);
+  };
+  return (
+    <div>
+      <h2>Long Converter</h2>
+      <div>
+        <label htmlFor="km">Km</label>
+        <input
+          value={flipped ? (amount * 1.60934).toFixed(4) : amount}
+          id="km"
+          placeholder="Km"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === true}
+        />
+      </div>
+      <div>
+        <label htmlFor="mile">mile</label>
+        <input
+          value={flipped ? amount : (amount / 1.60934).toFixed(4)}
+          id="mile"
+          placeholder="Mile"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === false}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>{flipped ? "Km => Mile" : "Mile => Km"}</button>
+    </div>
+  );
+}
+
+function App() {
+  const [index, setIndex] = React.useState("x");
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="x">Select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "x" ? "Please select your units" : null}
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
     </div>
   );
 }
